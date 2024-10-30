0b0e45fe90df9877375aa7b9e36bb8241e71331d
Julio González <juliogtrenard@gmail.com>
2024-10-30 22:15:14 +0100

    ADD movimiento del player

---
+++ Scripts/player.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+
+const SPEED = 6000.0
+
+func _physics_process(delta: float) -> void:
+	var direction = Input.get_vector("left", "right", "up", "down")
+	
+	if direction:
+		velocity = direction * SPEED * delta
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED * delta)
+		velocity.y = move_toward(velocity.y, 0, SPEED * delta)
+		
+	move_and_slide()
